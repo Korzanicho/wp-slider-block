8407b29b29d869cc677ba8b991a6b85ed4575fcb
Adrian <adrian.korzan@gmail.com>
2026-04-27 21:25:18 +0200

    feat: slides preview

--- slider/build/slider/index.asset.php
+++ slider/build/slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b7685dd381c58bb8266');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3f72e9d2386523899087');
